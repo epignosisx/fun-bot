77e6270e1e458adf723466b214d41efde6ee2ec6
roberto-mardeni <rmardeni@gmail.com>
2017-01-26 17:49:10 -0500

    Added courtesy hold method to check for availability

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { ApiAiAssistant } from "actions-on-google";
 import * as zillow from "./zillow";
+import * as ch from "./courtesy-hold";
 
 process.env.DEBUG = 'actions-on-google:*';
 
@@ -11,7 +12,19 @@ app.use(bodyParser.json({ type: 'application/json' }));
 
 app.get("/", (req: express.Request, res: express.Response) => {
     // zillow.getPropertyEstimate("7471 NW 112th Path", "33178", function (estimate: number) { });
-    
+
+    // var chrq = new ch.CourtesyHoldAvailabilityRequest();
+    // chrq.optionDate = "2017-01-27T05:00:00.000Z";
+    // chrq.numberOfCabins = 1;
+    // chrq.metaCode = "OB";
+    // chrq.itineraryCode = "BAD";
+    // chrq.rateCode = "PEG";
+    // chrq.sailingDate = "2018-06-08T04:00:00.000Z";
+    // chrq.shipCode = "VI";
+    // chrq.stateroomTypeCode = "VIOBNB";
+    // chrq.rank = 91;
+    // ch.checkIfAvailable(chrq, function (response: ch.CourtesyHoldAvailabilityResponse) { });
+
     res.send("Hello world!" + new Date());
 });
 
