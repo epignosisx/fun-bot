d69ac868cadc5891cebb3861c984a15545695a83
roberto-mardeni <rmardeni@gmail.com>
2017-01-26 16:43:23 -0500

    Added zillow module

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import { ApiAiAssistant } from "actions-on-google";
+import * as zillow from "./zillow";
 
 process.env.DEBUG = 'actions-on-google:*';
 
@@ -9,6 +10,8 @@ app.set('port', (process.env.PORT || 8080));
 app.use(bodyParser.json({ type: 'application/json' }));
 
 app.get("/", (req: express.Request, res: express.Response) => {
+    // zillow.getPropertyEstimate("7471 NW 112th Path", "33178", function (estimate: number) { });
+    
     res.send("Hello world!" + new Date());
 });
 
@@ -26,10 +29,10 @@ app.post("/", (req: express.Request, res: express.Response) => {
 
 // Start the server
 var server = app.listen(app.get('port'), function () {
-  console.log('App listening on port %s', server.address().port);
-  console.log('Press Ctrl+C to quit.');
+    console.log('App listening on port %s', server.address().port);
+    console.log('Press Ctrl+C to quit.');
 });
 
-function bookCruiseIntent(assistant: ApiAiAssistant){
+function bookCruiseIntent(assistant: ApiAiAssistant) {
     assistant.tell("Webhook response!!");
 }
